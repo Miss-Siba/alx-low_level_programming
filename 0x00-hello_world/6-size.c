40411d420fee4f0f7e41f6ffbd80d2930404fde3
Miss-Siba <sibaquma@gmail.com>
2023-09-07 10:52:23 -0700

    C is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,8 @@ int main(void)
 {
 	printf("Size of char: %zu bytes\n", sizeof(char));
 	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long: %zu byetes\n", sizeof(long));
+	printf("Size of long int: %zu byetes\n", sizeof(long int));
+	printf("Size of long longint: %zu bytes\n", sizeof(long long int));
 	printf("Sze of float: %zu bytes\n", sizeof(float));
 
 	return (0);
