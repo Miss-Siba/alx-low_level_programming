b153c5ce0cfbea3ddf200d0fbb1846f3ea7b644b
Miss-Siba <sibaquma@gmail.com>
2023-09-08 12:26:21 -0700

    C is fun

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - print all possible combinations of single-digit numbers.
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
