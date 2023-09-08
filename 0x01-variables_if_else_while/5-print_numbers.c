c1dff2f70e4dd5fb4e06ce6423ad0711015ff518
Miss-Siba <sibaquma@gmail.com>
2023-09-08 10:19:35 -0700

    C is fun

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints all single digits of base 10 from 0.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
