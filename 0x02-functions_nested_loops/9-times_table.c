beb88d0b49a54309fbffce7d89b58e0512c4d331
Miss-Siba <sibaquma@gmail.com>
2023-09-12 19:38:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,32 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, k, u, d;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-
-			if (j == 0)
-			{
-				_putchar(k + '0');
+		if (k > 9)
+		{
+			u = k % 10;
+			d = (k - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
-			if (k < 9 && j != 0)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (i != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
 				_putchar(k + '0');
-			}
-			else if (k >= 9)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 9) + '0');
-			_putchar((k % 9) + '0');
 			}
 		}
 		_putchar('\n');
