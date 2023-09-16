cacaa98b312fb158852f7a5a77a62de5608ec15b
Miss-Siba <sibaquma@gmail.com>
2023-09-16 03:55:12 -0700

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,39 +7,27 @@
  */
 void print_to_98(int n)
 {
+	int i, j;
 
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (i = n;i <= 98; i++)
 		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-						break;
+			if (i != 98)
+
+				printf("%d,", i);
+					else if (i == 98)
+						printf("%d\n", i);
 			}
-			else
-			{
-				printf("%d", n);
-			}
-		}
 	}
-	else
-	{
-		for (; n >= 98; n--)
+	else if (n >= 98)
 		{
-			if (n == 98)
+		for (j = n; j >= 98; j--)
 			{
-				printf("%d", n);
-				printf("\n");
-				break;
-				}
-				else
-				{
-				printf("%d", n);
-
-		}
-		}	
-		}
-
+			if (j != 98)
+				printf("%d", j);
+					else if (j == 98)
+						printf("%d\n", j);
+			}
+	}
 }
