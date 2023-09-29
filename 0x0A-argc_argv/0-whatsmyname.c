3cea1e4fbc46ee2e0253774b62206e0e49a88fdd
Miss-Siba <sibaquma@gmail.com>
2023-09-29 02:30:40 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints its own name.
+ * @argc: The number of command-line arguments (unused).
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[]) 
+{
+	(void)argc;
+	printf("The name of this program is: %s\n", argv[0]);
+	return (0);
+}
