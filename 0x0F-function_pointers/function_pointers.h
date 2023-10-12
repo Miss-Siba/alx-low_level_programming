b76c37407a2ac180af3a901b2aa5afa72a6ae5d8
Miss-Siba <sibaquma@gmail.com>
2023-10-12 06:30:48 -0700

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-int main(int __attribute__((__unused__)) argc, char *argv[]);
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
