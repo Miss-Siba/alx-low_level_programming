2ba3fc4ac448a6af04c4c0100eb2f7b26ff50fd7
Miss-Siba <sibaquma@gmail.com>
2023-10-19 13:05:04 -0700

    lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,11 @@
-section .data
-    hello db "Hello, Holberton",10 ; The string to be printed with a newline character (10)
 
-section .text
-    global main
-
-    extern printf
-    main:
-        push rbp
-        mov rdi, hello   ; Format string
-        call printf     ; Call the printf function
-        add rsp, 8      ; Restore the stack
-        xor rax, rax    ; Return 0
-        pop rbp
-        ret
+       global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
