7abf72014ade45ed9b7bca7166a228afb00e1cca
Miss-Siba <sibaquma@gmail.com>
2023-10-24 08:19:01 -0700

    more_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,14 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
+		return;
 
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		*head = temp;
+
 	}
 	*head = NULL;
 }
