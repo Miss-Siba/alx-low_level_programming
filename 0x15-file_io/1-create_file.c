8dcf76b6b6cfa69449eb0c3e5ae5c7a439fb3714
Miss-Siba <sibaquma@gmail.com>
2023-10-30 11:09:24 -0700

    file_i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,10 +19,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
 
 	for (new_letters = 0; text_content[new_letters]; new_letters++)
+		;
+
 	rwr = write(fd, text_content, new_letters);
 
 	if (rwr == -1)
