e9b4c62ab65c70c6019eef3de0fe3f014de05804
Miss-Siba <sibaquma@gmail.com>
2023-10-30 12:50:18 -0700

    file_i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if file can beopened.
- * @file_from: file from.
- * @file_to: file to.
- * @argv: arguments.
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -17,14 +17,15 @@ void error_file(int file_from, int file_to, char *argv[])
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
+
 /**
  * main - check the code for Holberton School students.
  * @argc: number of arguments.
- * @argv: arguments.
+ * @argv: arguments vector.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
@@ -38,7 +39,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-		file_from = open(argv[1], O_RDONLY);
+
+	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
