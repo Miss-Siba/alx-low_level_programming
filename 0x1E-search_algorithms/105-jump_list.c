0cafdd7e1673ebf639f8361b06ca446227689af8
Miss-Siba <sibaquma@gmail.com>
2024-04-10 11:07:40 -0700

    lists

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,40 +12,42 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-        size_t step, step_size;
-        listint_t *node, *jump;
 
-        /* Check if the list is NULL or empty */
-        if (list == NULL || size == 0)
-                return (NULL);
+	size_t step, step_size;
+	listint_t *node, *jump;
 
-        step = 0;
+	/* Check if the list is NULL or empty */
+	if (list == NULL || size == 0)
+		return (NULL);
 
-        /* Calculate the jump step size */
-        step_size = sqrt(size);
+	step = 0;
 
-        for (node = jump = list; jump->index + 1 < size && jump->n < value;)
-        {
-                /* Store the previous jump node */
-                node = jump;
+	/* Calculate the jump step size */
+	step_size = sqrt(size);
 
-                for (step += step_size; jump->index < step; jump = jump->next)
-                {
-                        /* If reached the end of the list */
-                        if (jump->index + 1 == size)
-                                break;
-                }
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		/* Store the previous jump node */
+		node = jump;
 
-                printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
-        }
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			/* If reached the end of the list */
+			if (jump->index + 1 == size)
 
-        printf("Value found between indexes [%ld] and [%ld]\n",
-                        node->index, jump->index);
+				break;
+		}
 
-        for (; node->index < jump->index && node->n < value; node = node->next)
-                printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 
-        printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-        return (node->n == value ? node : NULL);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
